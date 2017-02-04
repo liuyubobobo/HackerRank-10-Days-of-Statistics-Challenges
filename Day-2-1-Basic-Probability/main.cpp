0fb5404ed5a20ba62cc48714d3e067c7e919a7f7
liuyubobobo <liuyubobobo@gmail.com>
2017-02-04 19:24:46 +0800

    Day 2 Problem 1 solved.

---
+++ Day-2-1-Basic-Probability/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+int gcd( int a, int b){
+
+    assert( a > 0 && b > 0 );
+    if( a < b )
+        swap( a, b);
+
+    if( a % b == 0 )
+        return b;
+
+    return gcd( b, a%b );
+}
+
+int main() {
+
+    int total = 6*6;
+    int count = 0;
+    for( int i = 1 ; i <= 6 ; i ++ )
+        for( int j = 1 ; j <= 6 ; j ++ )
+            if( i + j <= 9 )
+                count ++;
+
+    int g = gcd( total, count );
+    cout<<count/g<<"/"<<total/g<<endl;
+
+    return 0;
+}
