df3bf0f877ff28894f2279c663bffcd5a93ca83a
liuyubobobo <liuyubobobo@gmail.com>
2017-02-05 12:59:58 +0800

    Day 3 Problem 1 solved.

---
+++ Day-3-1-Conditional-Probability/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // Given that we know one child is a boy,
+    // we have the following sample space:
+    // (Child1=Boy, Child2=Girl) (Child1=Girl, Child2=Boy) (Child1=Boy, Child2=Boy)
+    // Thus the probability of both children being boys is 1/3.
+    cout << "1/3" << endl;
+
+    return 0;
+}
