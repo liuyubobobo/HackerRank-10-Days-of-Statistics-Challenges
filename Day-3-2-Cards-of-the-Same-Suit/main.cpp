cdde2fcc4d68b1393050fe8d5d99809d2ecd1f32
liuyubobobo <liuyubobobo@gmail.com>
2017-02-05 13:24:21 +0800

    Day 3 Problem 2 solved.

---
+++ Day-3-2-Cards-of-the-Same-Suit/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // There are 1/4 chance to select one suit
+    // and then 12/51 chance to select another card of the same suit
+    // There are 4 suits in total
+    // so the result is 4 * 1/4 * 12/51 = 12/51
+
+    cout<<"12/51"<<endl;
+
+    return 0;
+}
